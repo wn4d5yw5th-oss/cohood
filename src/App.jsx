14000b0ce5d76d0d83ceab74f452c544f7ac118d
Serdar <serdar@Serdar-MacBook-Air.local>
2026-03-19 22:07:38 +0300

    iCloud butonu kaldirildi

--- src/App.jsx
+++ src/App.jsx
@@ -327,11 +327,7 @@ function Auth({ onLogin, lang, setLang }) {
             <svg width="17" height="17" viewBox="0 0 24 24"><path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4"/><path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853"/><path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l3.66-2.84z" fill="#FBBC05"/><path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335"/></svg>
             Gmail
           </button>
-          <button style={{ flex:1, padding:"11px 0", background:"#fff", border:"1.5px solid #E2D9CC", borderRadius:12, cursor:"pointer", fontSize:13, color:"#2C2416", fontWeight:600, display:"flex", alignItems:"center", justifyContent:"center", gap:8 }}>
-            <svg width="15" height="17" viewBox="0 0 814 1000" fill="#2C2416"><path d="M788.1 340.9c-5.8 4.5-108.2 62.2-108.2 190.5 0 148.4 130.3 200.9 134.2 202.2-.6 3.2-20.7 71.9-68.7 141.9-42.8 61.6-87.5 123.1-155.5 123.1s-85.5-39.5-164-39.5c-76 0-103.7 40.8-165.9 40.8s-105-37.5-155.5-127.4C46.7 790.7 0 663 0 541.8c0-207.3 136.4-317 272.8-317 70.1 0 128.4 46.4 172.1 46.4 41.8 0 107.2-49.1 185.6-49.1zm-194.3-86.5c33.3-40.4 57.3-96.4 57.3-152.4 0-7.8-.6-15.6-2-22.4-54.4 2-118.8 36.3-157.8 79.4-31.4 35.1-59.1 90.3-59.1 147.1 0 8.4 1.4 16.9 2 19.5 3.2.6 8.4 1.3 13.6 1.3 48.7 0 109.3-32.7 145.9-72.5z"/></svg>
-            iCloud
-          </button>
-        </div>
+      
         <div style={{ textAlign:"center", marginTop:20, fontSize:13, color:"#6B5E4E" }}>
           {t.noAcc}{" "}
           <button onClick={()=>setScreen("register")} style={{ background:"none", border:"none", color:G, fontWeight:700, cursor:"pointer", fontSize:13, fontFamily:"DM Sans,sans-serif" }}>{t.register}</button>
